0dfdeaef44cb73d10313e9faf8b536e53232de57
t-dever <91967792+t-dever@users.noreply.github.com>
2024-02-09 11:45:35 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,9 +1,1 @@
-module "password_reset" {
-  source              = "modules/aviatrix_credential_rotation"
-  controller_dns_name = var.script_input_data.aviatrix_controller_hostname
-  account_reset       = {
-    name     = ""  # Provide the Name of the Account
-    password = ""  # Provide the current password
-    email    = ""  # Provide the email address for that account
-  }
-}
+
